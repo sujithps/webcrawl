4620848719076291948e6bac13f9c86f716b9b07
Sujith PS <sujithps534@gmail.com>
2019-10-03 20:50:14 +0530

    Sujith | List all the details of a twitter profile

--- app/controllers/tweet_streams_controller.rb
+++ app/controllers/tweet_streams_controller.rb
@@ -1,18 +1,21 @@
 class TweetStreamsController < ApplicationController
   attr_accessor :twitter_client
+
   def new
   end
 
   def create
     @user_name = params[:twitter_handle]
     @tweets = twitter_client.user_timeline(@user_name)
-    @follower_count = twitter_client.user(@user_name).followers_count
+    @user_details = twitter_client.user(@user_name)
+
+    @follower_count = @user_details.followers_count
   end
 
   def twitter_client
     @twitter_client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key        = "QmnF0N1nALWJCshk5ecUWXG2n"
-      config.consumer_secret     = "N7PoX41YI7YHinZw5qI9XmOskX0nzw60GPtMuQGb7UY6oNh853"
+      config.consumer_key = "QmnF0N1nALWJCshk5ecUWXG2n"
+      config.consumer_secret = "N7PoX41YI7YHinZw5qI9XmOskX0nzw60GPtMuQGb7UY6oNh853"
     end
   end
 end
